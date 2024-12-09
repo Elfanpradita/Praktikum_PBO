82d7218789c99dd7fac2044d2fb685893818a162
Elfanpradita <71947752+Elfanpradita@users.noreply.github.com>
2024-12-09 21:45:33 +0700

    Delete Sesi11 directory

--- Sesi11/Vehicle/VehicleTester.java
+++
@@ -1,9 +0,0 @@
-public class VehicleTester {
-    public static void main(String[] args) {
-        // Membuat objek Car
-        Car car = new Car("Toyota", 4);
-
-        // Menampilkan informasi kendaraan
-        car.displayInfo("Sedan"); // Output: Brand: Toyota, Type: Sedan, Doors: 4
-    }
-}
